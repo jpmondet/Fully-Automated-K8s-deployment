8387fcc8081e920a27b1f80d3bbff447fcf26317
jpmondet <jp@mondet.org>
2018-10-02 22:11:04 +0200

    Add delegate_to for the dns apply

--- deploy_all.sh
+++ deploy_all.sh
@@ -15,12 +15,14 @@ ansible-playbook deploy_network.yml -e "option=5549"
 wait
  
 cd ../Fully-automated-k8s-deployment
+wait
 git checkout 1.12
 wait
  
 # Bypassing an issue with netaddr
 # Cause it has been installed with sudo by CITC.
 sudo pip uninstall -y netaddr
+wait
 pip install --user netaddr
 wait
  
